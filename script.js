1865dfe0ab95f71de3396b0f747213d5377dbbd3
Marius <mariusategbo3@gmail.com>
2025-05-09 19:42:59 +0100

    Premier commit du site PackStream

---
+++ script.js
@@ -0,0 +1,6 @@
+// JavaScript pour booster l'engagement (ex: relance après délai)
+setTimeout(() => {
+  const btn = document.querySelector('.btn-whatsapp');
+  btn.style.backgroundColor = '#ff5f6d';
+  btn.innerText = "Clique ici avant la fin de l'offre !";
+}, 10000); // 10 sec
